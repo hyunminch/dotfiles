9ef62626cf59c90889c7d9803efb91cc0be7b405
Hyun Min Choi <hyunmin.personal@gmail.com>
2018-03-24 16:02:54 +0900

    Implement neovim setup script

---
+++ nvim/init.vim
@@ -0,0 +1,38 @@
+set number
+set enc=utf-8
+set cursorline
+set mouse=a
+
+set expandtab
+set smarttab
+set shiftwidth=2
+set softtabstop=2
+
+" buffer navigation key mappings
+set hidden
+nnoremap <C-N> :bnext<CR>
+nnoremap <C-P> :bprev<CR>
+
+" find files
+nnoremap <S-F> :Files<CR>
+
+set backspace=indent,eol,start
+set wildignore+=*.class,*.swp
+
+call plug#begin('~/.local/share/nvim/plugged')
+
+" for better looks
+Plug 'dracula/vim', { 'as': 'dracula' }
+Plug 'vim-airline/vim-airline'
+Plug 'vim-airline/vim-airline-themes'
+
+Plug 'ap/vim-buftabline'
+Plug 'junegunn/fzf', { 'dir': '~/.fzf', 'do': './install --all' }
+Plug 'junegunn/fzf.vim'
+
+Plug 'derekwyatt/vim-scala'
+Plug 'posva/vim-vue'
+
+call plug#end()
+
+colorscheme dracula 
---
+++ setup-neovim.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+echo "Setting up neovim..."
+
+# Install neovim
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  distro = $(gawk -F= '/^NAME/{ print $2 }' /etc/os-release)
+  if [[ distro == "Ubuntu" ]]; then
+    sudo apt-get install software-properties-common
+    sudo add-apt-repository ppa:neovim-ppa/unstable
+    sudo apt-get update
+    sudo apt-get install neovim
+  else
+    echo "Only supports Ubuntu distros. Aborting setup."
+    exit 1
+  fi
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  brew install neovim
+else
+  exit 1
+fi
+
+# Configure vim-plug
+echo "Setting up vim-plug..."
+
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Copy neovim configuration
+echo "Configuring init.vim..."
+mkdir -p ~/.config/nvim
+cp nvim/init.vim ~/.config/nvim/init.vim
+
+# Create a symbolic link of init.vim at ~/.nvimrc
+# Does not make a link when ~/.nvimrc already exists
+echo "Creating a symbolic link for init.vim at ~/.nvimrc..."
+if [[ ! -f ~/.nvimrc ]]; then
+  ln -s ~/.config/nvim/init.vim ~/.nvimrc
+else
+  echo "~/.nvimrc already exists. Omitting the symbolic link process."
+fi
+
+# Install neovim plugins
+echo "Installing neovim plugins..."
+nvim --headless +PlugInstall +qall
+
+echo "neovim setup complete!"
